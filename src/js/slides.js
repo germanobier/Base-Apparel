35b0cbf789412cae75550a1fc02125dabea14d16
Germano Bier <germanobier@hotmail.com>
2021-06-23 16:16:50 -0300

    added product

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container-1', {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
@@ -6,11 +6,25 @@ var swiper = new Swiper('.swiper-container', {
     disableOnInteraction: false,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-1',
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next-1',
+    prevEl: '.swiper-button-prev-1',
+  },
+});
+
+var swiper2 = new Swiper('.swiper-container-2', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination-2',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
   },
 });
