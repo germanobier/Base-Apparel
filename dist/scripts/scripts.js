55bcc45a8f8324332f161eb94d27f8489ccf7224
Germano Bier <germanobier@hotmail.com>
2021-06-23 01:18:34 -0300

    first-commit

---
+++ dist/scripts/scripts.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".swiper-container",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
